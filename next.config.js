615ea02a05cadd1536fc0eabdb47b433a1c6c888
Eric Lin <40213852eric@gmail.com>
2024-03-13 15:33:50 +0800

    deploy: deploy to pages branch

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.module.rules.push({
-      contract: /src/contracts,
-      loader: "ignore-loader",
-    });
     return config;
   },
   
