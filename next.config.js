160158f555b1871bf6d89e1d332a1fc54d126b54
Eric Lin <40213852eric@gmail.com>
2024-03-13 14:48:41 +0800

    feat: skeleton

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.module.rules.push({
+      contract: src/contracts,
+      loader: "ignore-loader",
+    });
     return config;
   },
+  
 };
 
 module.exports = nextConfig;
